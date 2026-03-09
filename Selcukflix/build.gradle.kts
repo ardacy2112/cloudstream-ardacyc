84b2be28892cc0e5ac2e3b5c49de530661fa7510
Ardac <138930253+ardacy2112@users.noreply.github.com>
2026-03-09 12:12:00 +0300

    Add files via upload

---
+++ Selcukflix/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+}
+
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "selcukflix.net film ve dizi kaynagi"
+    authors = listOf("ardacy2112")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "tr"
+    iconUrl = "https://selcukflix.net/wp-content/uploads/2023/10/logo.png"
+}
