47f84cb26b043cc6467f71a00a1e9240af139ab2
Ardac <138930253+ardacy2112@users.noreply.github.com>
2026-03-09 12:13:25 +0300

    Add files via upload

---
+++ HDFilmCehennemi/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+}
+
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "hdfilmcehennemi.nl film ve dizi kaynagi"
+    authors = listOf("ardacy2112")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "tr"
+    iconUrl = "https://hdfilmcehennemi.nl/wp-content/uploads/2022/11/logo.png"
+}
